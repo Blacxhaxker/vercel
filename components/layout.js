bf326957adb42747f6334e4e3b038978079c4040
Aomsin <blacxhaxker@gmail.com>
2021-08-03 17:43:56 +0700

    add frame

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,21 @@
+import Head from "next/head";
+import Image from "next/image";
 import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>;
+const name = "Aomsin";
+export const siteTitle = "Aomsin";
+
+export default function Layout({ children, home }) {
+  return (
+    <iframe
+      width="600"
+      height="450"
+      src="https://datastudio.google.com/embed/reporting/fdfe6c09-3e88-475a-b172-a545520bfd67/page/R9nWC"
+      frameborder="0"
+      style="border:0"
+      allowfullscreen
+    ></iframe>
+  );
 }
